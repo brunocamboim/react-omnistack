3b4ce491b3eda9225d66facb0f29d860db9c7a08
Bruno Camboim <bruno@estudiosis.com.br>
2019-08-09 11:37:29 -0300

    Criação

---
+++ backend/src/routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const DevController = require('./controllers/DevController')
+
+const routes = express.Router();
+
+// //GET, POST, PUT, DELETE
+// routes.get('/', (req, res) => {
+//     return res.json({ message: `Hello ${req.query.name}`})
+// });
+
+routes.post('/devs', DevController.store)
+
+module.exports = routes;
